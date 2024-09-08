a89bb44c3fea688d017eb1c42b8550cd2ed55b17
moduloscript <maduemeka254@gmail.com>
2024-09-08 17:38:36 +0100

    enabled credential password signup sequence

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import {
+  convexAuthNextjsMiddleware,
+  createRouteMatcher,
+  isAuthenticatedNextjs,
+  nextjsMiddlewareRedirect,
+} from "@convex-dev/auth/nextjs/server";
+
+const isPublicPage = createRouteMatcher(["/auth"]);
+
+
+const isProtectedRoute = createRouteMatcher(["/product(.*)"]);
+ 
+export default convexAuthNextjsMiddleware((request) => {
+  if (!isPublicPage(request) && !isAuthenticatedNextjs()) {
+    return nextjsMiddlewareRedirect(request, "/auth");
+  }
+  //💥🧑‍🎤 Redirect User away from "/auth" if Authenticated
+  if (isPublicPage(request) && isAuthenticatedNextjs()) {
+    return nextjsMiddlewareRedirect(request, "/");
+  }
+});
+
+export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
